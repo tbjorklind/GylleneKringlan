5eaf764a838cf32829fa17e99b29049792d134fc
Tanja Björklind <tbjorklind@gmail.com>
2023-04-13 22:33:30 +0200

    Firebase

---
+++ config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDz7EamseqZGm5_GQVw_OxGkSloutcT1XQ',
+  authDomain: 'gyllende-kringlan.firebaseapp.com',
+  databaseURL:
+    'https://gyllende-kringlan-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'gyllende-kringlan',
+  storageBucket: 'gyllende-kringlan.appspot.com',
+  messagingSenderId: '539257040801',
+  appId: '1:539257040801:web:8b77c9a29a14fbce6025e0',
+  measurementId: 'G-12H4VPPR93'
+}
+
+firebase.initializeApp(firebaseConfig);
---
+++ firebase.js
@@ -0,0 +1,69 @@
+const db = firebase.firestore()
+
+// --------------------- GET ----------------------
+async function getCollectionFromFirestore (collectionName) {
+  let arr = []
+  let ref = await db
+    .collection(collectionName)
+    .get()
+    .then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        arr.push({
+          data: doc.data()
+        })
+      })
+    })
+
+  //   console.log(arr)
+
+  return arr
+}
+
+async function getDocumentFromFirestore (collectionName, id) {
+  let ref = await db.collection(collectionName).doc(id).get()
+  let data = await ref.data()
+  //   console.log(data.backpack1.riddles)
+  return data
+}
+
+// console.log(getCollectionFromFirestore('Teams'))
+// console.log(getDocumentFromFirestore('Teams', 'Team1'))
+
+// --------------------- ADD -----------------------
+async function addDocumentToFirebase (collectionName) {
+  let ref = await db.collection(collectionName).add({})
+  let newDocumentID = await ref.id
+
+  //   console.log(newDocumentID)
+
+  return newDocumentID
+}
+
+// console.log(addDocumentToFirebase('Users'))
+// --------------------- UPDATE ----------------------
+
+async function updateDocumentToFirebase (collectionName, id, data = {}) {
+  let document = await getDocumentFromFirestore('Teams', id)
+  let newUserID = await addDocumentToFirebase('Users')
+  document.users = [...document.users, newUserID]
+  await db.collection(collectionName).doc(id).update(document)
+}
+
+// console.log(updateDocumentToFirebase('Teams', 'Team1'))
+
+// --------------------- DELETE -----------------------
+
+async function deleteDocumentFromFirestore (collectionName, id) {
+  let ref
+  try {
+    ref = await db.collection(collectionName).doc(id).delete()
+    console.log('Document successfully deleted!')
+  } catch (error) {
+    console.error('Error removing document: ', error)
+  }
+  return ref
+}
+
+// console.log(deleteDocumentFromFirestore('Teams', 'Team1'))
+
+// ------------------------------------------------------
