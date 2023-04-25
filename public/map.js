e5ca3fb5ca59b11ee4ad89f32de149020c1325ca
tbjorklind <97900554+tbjorklind@users.noreply.github.com>
2023-04-25 15:58:02 +0200

    Styling map

--- public/map.js
+++ public/map.js
@@ -1,10 +1,14 @@
-let zones = [
+// Ändra locations till rätt lag, glöm ej att den för båda ska finnas på båda.
+let storylineOne = [
   { lat: 55.6095466, lng: 12.9937084 },
   { lat: 55.6097507, lng: 12.9962505 },
   { lat: 55.6078666, lng: 12.9910128 },
   { lat: 55.6026459, lng: 12.9929222 },
   { lat: 55.6028939, lng: 13.0011858 },
-  { lat: 55.6059576, lng: 13.0010688 },
+  { lat: 55.6059576, lng: 13.0010688 }
+]
+
+let storylineTwo = [
   { lat: 55.6082255, lng: 13.0100072 },
   { lat: 55.6024005, lng: 12.9853929 },
   { lat: 55.6049379, lng: 13.0091124 },
@@ -13,51 +17,187 @@ let zones = [
   { lat: 55.5966309, lng: 12.996344 },
   { lat: 55.5942211, lng: 13.001169 }
 ]
+
+const styledMapType = new google.maps.StyledMapType(
+  [
+    { elementType: 'geometry', stylers: [{ color: '#ebe3cd' }] },
+    { elementType: 'labels.text.fill', stylers: [{ color: '#523735' }] },
+    { elementType: 'labels.text.stroke', stylers: [{ color: '#f5f1e6' }] },
+    {
+      featureType: 'administrative',
+      elementType: 'geometry.stroke',
+      stylers: [{ color: '#c9b2a6' }]
+    },
+    {
+      featureType: 'administrative.land_parcel',
+      elementType: 'geometry.stroke',
+      stylers: [{ color: '#dcd2be' }]
+    },
+    {
+      featureType: 'administrative.land_parcel',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#ae9e90' }]
+    },
+    {
+      featureType: 'landscape.natural',
+      elementType: 'geometry',
+      stylers: [{ color: '#dfd2ae' }]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'geometry',
+      stylers: [{ color: '#dfd2ae' }]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#93817c' }]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'geometry.fill',
+      stylers: [{ color: '#a5b076' }]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#447530' }]
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [{ color: '#f5f1e6' }]
+    },
+    {
+      featureType: 'road.arterial',
+      elementType: 'geometry',
+      stylers: [{ color: '#fdfcf8' }]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry',
+      stylers: [{ color: '#f8c967' }]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry.stroke',
+      stylers: [{ color: '#e9bc62' }]
+    },
+    {
+      featureType: 'road.highway.controlled_access',
+      elementType: 'geometry',
+      stylers: [{ color: '#e98d58' }]
+    },
+    {
+      featureType: 'road.highway.controlled_access',
+      elementType: 'geometry.stroke',
+      stylers: [{ color: '#db8555' }]
+    },
+    {
+      featureType: 'road.local',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#806b63' }]
+    },
+    {
+      featureType: 'transit.line',
+      elementType: 'geometry',
+      stylers: [{ color: '#dfd2ae' }]
+    },
+    {
+      featureType: 'transit.line',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#8f7d77' }]
+    },
+    {
+      featureType: 'transit.line',
+      elementType: 'labels.text.stroke',
+      stylers: [{ color: '#ebe3cd' }]
+    },
+    {
+      featureType: 'transit.station',
+      elementType: 'geometry',
+      stylers: [{ color: '#dfd2ae' }]
+    },
+    {
+      featureType: 'water',
+      elementType: 'geometry.fill',
+      stylers: [{ color: '#b9d3c2' }]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [{ color: '#92998d' }]
+    }
+  ],
+  { name: 'Styled Map' }
+)
+
 var options = {
   enableHighAccuracy: true,
   timeout: 5000,
   maximumAge: 0
 }
 
-var map
+// LOCAL STORAGE
+let index = 0
+let storyline = storylineOne
+
+// Ska göras från render-storyline sen..
+function renderFarwel () {
+  navigator.geolocation.getCurrentPosition(initMap)
+}
+renderFarwel()
+
 function initMap (position) {
   var crd = position.coords
-
-  console.log(`More or less ${crd.accuracy} meters.`)
-
+  var map
   // Creates a map centered on the current location
   map = new google.maps.Map(document.getElementById('map'), {
     center: {
       lat: crd.latitude,
       lng: crd.longitude
     },
-    zoom: 15
+    zoom: 25,
+    mapTypeControlOptions: {
+      mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'styled_map']
+    }
+  })
+  // Hämta local
+  let index = 0
+  let storyline = storylineOne
+
+  let circle = new google.maps.Circle({
+    radius: 100,
+    map: map,
+    center: {
+      lat: storyline[index].lat,
+      lng: storyline[index].lng
+    },
+    strokeColor: '#CC813A',
+    strokeOpacity: 0.8,
+    strokeWeight: 3,
+    fillColor: '#CC813A',
+    fillOpacity: 0.35
+  })
+  circle.setMap(map)
+
+  const img = {
+    url: '../images/boll.png',
+    scaledSize: new google.maps.Size(300, 300)
+  }
+
+  // Creates a marker at the current location
+  const marker = new google.maps.Marker({
+    position: { lat: crd.latitude, lng: crd.longitude },
+    map: map,
+    title: 'Your location',
+    icon: img
   })
 
-  for (let zone of zones) {
-    let circle = new google.maps.Circle({
-      radius: 100,
-      map: map,
-      center: {
-        lat: zone.lat,
-        lng: zone.lng
-      },
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 3,
-      fillColor: '#FF0000',
-      fillOpacity: 0.35
-    })
-
-    // Creates a marker at the current location
-    const marker = new google.maps.Marker({
-      position: { lat: crd.latitude, lng: crd.longitude },
-      map: map,
-      title: 'Your location'
-    })
-
-    circle.setMap(map)
-  }
+  //Associate the styled map with the MapTypeId and set it to display.
+  map.mapTypes.set('styled_map', styledMapType)
+  map.setMapTypeId('styled_map')
 }
 
-navigator.geolocation.getCurrentPosition(initMap)
+window.initMap = initMap
+
