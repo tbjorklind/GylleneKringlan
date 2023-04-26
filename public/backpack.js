7b3a1ef20f2e2a4e41661dada6b28aa6946466b0
My Bergquist Rajic <mybergqvist@gmail.com>
2023-04-26 12:35:58 +0200

    Plan for backpack & ending

---
+++ public/backpack.js
@@ -0,0 +1,9 @@
+"use strict"
+
+/*
+Tror det klokaste är att ha både ryggsäcksikonen och vyn när man klickar på /öppnar ryggsäcken
+hårdkodad i index.html, eftersom att den iprincip alltid ska vara åtkomlig. Så då räcker det att
+man sätter hidden på båda två tills de väl ska användas..
+
+Båda bör nog vara position absolute, för att det inte ska fucka med allt annat :D
+*/
---
+++ public/render-ending.js
@@ -0,0 +1,12 @@
+"use strict"
+
+/* 
+När man väl är framme vid slutet ska:
+    - Alla karaktärer rendreras och man uppmanas att välja sin tjuv 
+      OBS!! Endast på EN av mobilerna!! Annars kan dom ju gissa två ggr, å det vill vi inte... väl?
+    – Om man gissar rätt visas det att man lyckas
+    – Om man gissar fel får man frågan om man vill muta för en till gissning 
+        – Om man gissar rätt visas samma som ovan, att man lyckats
+        – Om man gissar fel igen eller accepterar nederlaget visas det att man förlorat  
+    – Och tillsist en vy om man inte hinner klart innan tiden är slut
+*/
