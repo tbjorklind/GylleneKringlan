6a7a082d0fb7b6a9c23c3ced3d67525d9cc5001b
My Bergquist Rajic <mybergqvist@gmail.com>
2023-05-06 17:52:19 +0200

    bakcup

--- public/render-ending.js
+++ public/render-ending.js
@@ -10,3 +10,89 @@ När man väl är framme vid slutet ska:
         – Om man gissar fel igen eller accepterar nederlaget visas det att man förlorat  
     – Och tillsist en vy om man inte hinner klart innan tiden är slut
 */
+
+const characterImages = [
+  {
+    img: "https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png",
+    name: "Anita"
+  },
+  {
+    img: "https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png",
+    name: "Tage"
+  },
+  {
+    img: "https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png",
+    name: "Von Asklund"
+  },
+  {
+    img: "https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png",
+    name: "Freja"
+  },
+  {
+    img: "https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png",
+    name: "Anita"
+  },
+  {
+    img: "https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png",
+    name: "Anita"
+  },
+  {
+    img: "https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png",
+    name: "Anita"
+  },
+  {
+    img: "https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png",
+    name: "Anita"
+  },
+  {
+    img: "https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png",
+    name: "Anita"
+  }
+]
+
+
+function renderCharacterAlternatives() {
+  // Header
+  let header = document.createElement("div")
+  header.id = "characterHeader";
+  header.style.textAlign = 'center';
+  header.innerHTML = "<span style='font-size: 4.5vw'>Vem har tagit den Gyllene Kringlan?</span><br><span style='font-size: 4.5vw;'>Välj er misstänkta tjuv!</span>"
+  document.getElementById("wrapper").appendChild(header);
+
+  let characterWrapper = document.createElement("div");
+  characterWrapper.id = "characterWrapper"
+  document.getElementById("wrapper").appendChild(characterWrapper);
+
+  characterImages.forEach(c => {
+    let characterDiv = document.createElement("div");
+    let characterImg = document.createElement("div");
+    let characterName = document.createElement("div");
+    characterImg.style.backgroundImage = `url(${c.img})`;
+    characterName.innerHTML = c.name;
+
+    characterDiv.appendChild(characterImg);
+    characterDiv.appendChild(characterName);
+    characterWrapper.appendChild(characterDiv);
+
+    characterDiv.addEventListener('click', () => { renderConfirmCharacter(c) })
+  })
+}
+
+function renderConfirmCharacter(character) {
+  let popup = document.createElement("div");
+  popup.id = 'confirmCharacter';
+  popup.innerHTML = `
+  <div>
+    <img src="${character.img}">
+    <div>Är du säker på att ${character.name} är tjuven?</div>
+    <div id="confirmCharacterBtns">
+      <div>JA</div>
+      <div>NEJ</div>
+    </div>
+  </div>
+  `;
+
+  document.querySelector("body").appendChild(popup);
+}
+
+renderCharacterAlternatives()
