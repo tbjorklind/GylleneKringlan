412bd5a514e3a2e5cf6d0500266b67413fe5ad85
My Bergquist Rajic <mybergqvist@gmail.com>
2023-05-06 16:17:08 +0200

    startup fix

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+import startup from "./startup.js"
 // const arrow = document.querySelector('.arrow')
 // const speed = document.querySelector('.speed-value')
 
@@ -62,9 +63,50 @@ let interval = setInterval(() => {
   }
 }, 1000)
 
+// Height
 const appHeight = () => {
   const doc = document.documentElement
   doc.style.setProperty('--app-height', `${window.innerHeight}px`)
 }
 window.addEventListener('resize', appHeight)
 appHeight()
+
+// Initiering av app
+function onLaunch() {
+  // If new user (no userId exists in local storage)
+  if (!localStorage.getItem('userId')) {
+    //startup()
+  }
+  // If the user is returning 
+  else {
+    // Ladda in kartan / kolla position
+    // Kör som vanligt: om INTE inom zon, visa kartan. om INOM zon, renderera storyline/karaktär/fråga
+    /*  PROBLEM: hur göra så att endast en mobil/person kan svara på frågorna.
+        Kanske fortfarande uppmana att bara en person har mobil, men att man 
+        kan ta in en ny mobil om den ena skulle dö. Sålänge dom bara spelar på 
+        en mobil, typ.
+        En lösning hade kunnat vara att när alla är inom zonen så blir en av
+        personerna/mobilerna valda till att svara på frågorna. Detta innebär
+        dock att vi måste "skicka ut" något, vilket vi inte vet hur man gör.
+        Vi vet ju bara hur man hämtar saker och ting. 
+        Ett annat alternativ hade varit att den första personen som kommer in
+        i zonen får svara på frågorn på sin mobil. Dock innebär detta att man
+        på något sätt måste skicka till databasen att denna personen är "utvald".
+        Och om alla går in i zonen samtidigt så kommer "fetchandet" gentemot 
+        datbasen kanske inte hinna med? Alternativt att när en person kommer
+        innanför zonen kollar man i databasen: är någon person utvald för denna
+        zonen? Om nej, så blir den personen vald och man får upp frågorna. Om ja
+        så ska det typ stå på skärmen "någon annan i gruppen svarar på detta redan".
+        Men för att dessa "passiva" användare/mobiler sen ska kunna ta sig vidare
+        i spelet så måste vi konstant kolla databasen om storyChapter har ändrats
+        (alltså att man har svarat klart på frågorna och ska vidare), eftersom att vi
+        som sagt inte vet hur man skickar ut nått utan endast kan hämta. Ska man
+        köra en set-timeout en gång i sekunden då, eller vad?
+        Pilligt som fan! hihi! <3 
+     */
+  }
+  startup()
+
+}
+
+onLaunch()
