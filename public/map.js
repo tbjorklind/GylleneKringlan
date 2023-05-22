879bda2d0370c7cf9180f89fefae70404fdf1296
My Bergquist Rajic <mybergqvist@gmail.com>
2023-05-22 18:29:38 +0200

    Changed coordinates

--- public/map.js
+++ public/map.js
@@ -168,9 +168,9 @@ async function initMap(position) {
       { lat: 55.6095466, lng: 12.9937084 }, // Badaren Anita
       { lat: 55.6097507, lng: 12.9962505 }, // Fiskaren Hansson
       { lat: 55.6059576, lng: 13.0010688 }, // Apotekaren Ruth
-      { lat: 55.6082255, lng: 13.0100072 }, // Dörrvakten Tor
+      { lat: 55.607938, lng: 13.010062 }, // Dörrvakten Tor – tidigare: lat: 55.6082255, lng: 13.0100072
       { lat: 55.6028939, lng: 13.0011858 }, // Torgaren Tage
-      { lat: 55.6059576, lng: 13.0010688 }, // Museum-ägaren Von
+      { lat: 55.604371, lng: 13.009306 }, // Museum-ägaren Von – tidigare: lat: 55.6059576, lng: 13.0010688 
       { lat: 55.603297, lng: 13.010903 }, // Detektiven August
       { lat: 55.598236, lng: 13.006477 }, // Hemlöse Roland
       { lat: 55.5942211, lng: 13.001169 } // Prästen Adolfsson
@@ -182,11 +182,11 @@ async function initMap(position) {
       { lat: 55.6095466, lng: 12.9937084 }, //Badaren anita
       { lat: 55.6078666, lng: 12.9910128 }, // Kötthandlaren Clemens
       { lat: 55.6026459, lng: 12.9929222 }, // Spelaren Carl-Wilhelm
-      { lat: 55.6024005, lng: 12.9853929 }, // Paret Charlie & Freja
+      { lat: 55.605159, lng: 12.998783 }, // Paret Charlie & Freja – tidigare: lat: 55.6024005, lng: 12.9853929 
       { lat: 55.6028939, lng: 13.0011858 }, // Torgaren Tage
       { lat: 55.6002879, lng: 13.0010339 }, // Konditorn Hilda
-      { lat: 55.600768, lng: 12.9940654 }, // Bibliotikarie Barbro
-      { lat: 55.5966309, lng: 12.996344 }, // Operasångerskan Birgit
+      { lat: 55.600747, lng: 12.995543 }, // Bibliotikarie Barbro – tidigare: lat: 55.600768, lng: 12.9940654 
+      { lat: 55.596947, lng: 12.996162 }, // Operasångerskan Birgit – tidigare: lat: 55.5966309, lng: 12.996344 
       { lat: 55.5942211, lng: 13.001169 } // Prästen Adolfsson
     ]
   }
